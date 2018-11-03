5d2a72e702a27eec75d6842dd10f9b887f53604a
Edilson Ngulele <edilsonngulele@gmail.com>
2018-11-03 20:43:10 +0200

    inital commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Edilson Ngulele on 11/3/18.
+//  Copyright © 2018 Edilson Ngulele. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
