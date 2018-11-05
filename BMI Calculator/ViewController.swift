66f5d679a8f63f0c32506d6c42d348c8784057db
Edilson Ngulele <edilsonngulele@gmail.com>
2018-11-05 23:54:11 +0200

    feat: add action to Calulate button
    When the Calculate button is pressed the BMI is calculated.

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     var valueBmi : Float = 0.0;
     var userName : NSString = "";
     var age : Float = 0.0;
-
+    
     
     @IBOutlet weak var textFieldUserName: UITextField!
     
@@ -27,24 +27,66 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
     }
-
+    
     
     @IBAction func resetFields(_ sender: Any) {
         textFieldUserName.text = nil;
         textFieldHeight.text = nil;
         textFieldWeight.text = nil;
         
+    }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "Alert", message: "Complete all fields", preferredStyle: UIAlertControllerStyle.alert);
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil));
+        self.present(alert, animated: true, completion: nil);
+    }
+    
+    @IBAction func calculateBmi(_ sender: Any) {
+        
+        guard let sUserName = self.textFieldUserName.text,
+            self.textFieldUserName.text?.isEmpty == false else{
+                showAlert()
+                return
+        }
+        
+        guard let sHeight = self.textFieldHeight.text,
+            self.textFieldHeight.text?.isEmpty == false else {
+                showAlert()
+                return
+        }
+        
+        guard let sWeight = self.textFieldWeight.text,
+            self.textFieldWeight.text?.isEmpty == false  else{
+                showAlert()
+                return
+        }
+        
+        guard let sAge = self.textFieldAge.text,
+            self.textFieldAge.text?.isEmpty == false else{
+                showAlert()
+                return
+        }
+        
+        
+        let heightFloat = (sHeight as NSString).floatValue;
+        let weightFloat = (sWeight as NSString).floatValue;
+        let ageFloat = (sAge as NSString).floatValue;
+        
+        
+        valueBmi = (weightFloat / (heightFloat * heightFloat))
+        userName = sUserName as NSString;
+        age = ageFloat;
         
     }
     
-
+    
 }
