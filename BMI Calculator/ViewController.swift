a918cc274c215e5aba73ea27047db7c1eb240627
Edilson Ngulele <edilsonngulele@gmail.com>
2018-11-05 23:08:49 +0200

    feat: add reset action
    Reset button clears all the textfields setting their value to nil

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    
+    var valueBmi : Float = 0.0;
+    var userName : NSString = "";
+    var age : Float = 0.0;
 
+    
+    @IBOutlet weak var textFieldUserName: UITextField!
+    
+    @IBOutlet weak var textFieldHeight: UITextField!
+    
+    @IBOutlet weak var textFieldWeight: UITextField!
+    
+    @IBOutlet weak var textFieldAge: UITextField!
+    
+    
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +37,14 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func resetFields(_ sender: Any) {
+        textFieldUserName.text = nil;
+        textFieldHeight.text = nil;
+        textFieldWeight.text = nil;
+        
+        
+    }
+    
 
 }
